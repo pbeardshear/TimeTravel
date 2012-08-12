1ac68144bbd794ac9a0711417d23f2661046be4c
Peter Beardshear <pbeardshear@berkeley.edu>
2012-08-11 20:27:53 -0700

    so the disgrace

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ io.sockets.on('connection', function (socket) {
 		// This won't work until we get an API key from Twilio...
 		http.request({
 			host: 'api.twilio.com',
-			path: '/2010-04-01/Accounts/AC85649c3c22e709eefda8fb2ec46a4192/SMS/Messages/[SMSMessageSid].json',
+			path: '/2010-04-01/Accounts/AC85649c3c22e709eefda8fb2ec46a4192/SMS/Messages.json',
 			method: 'GET'
 		});
 	});
