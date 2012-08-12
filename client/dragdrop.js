bff33657ee9b6473928682a338c27b05a45fa949
Bryan Chu <bryanchu08@yahoo.com>
2012-08-11 19:13:25 -0700

    it should be good

--- client/dragdrop.js
+++ client/dragdrop.js
@@ -38,15 +38,30 @@ Page = (function() {
 
 	return {
 		init: function() {
-			  var socket = io.connect('http://localhost');
-			  socket.on('news', function (data) {
-				console.log(data);
-				socket.emit('my other event', { my: 'data' });
-			  });
-			  socket.on('response', function (data) {
-				newFileUploaded(data);
-			  });
-			// window.addEventListener("keypress", newFileUploaded);
+			  filepicker.setKey('AjB_5ggM9QMO_uSoMgHNmz');
+				  filepicker.makeDropPane($('#drop')[0], {
+					dragEnter: function() {
+						console.log('enter');
+						$("#drop").html("Drop to upload").css({
+							'backgroundColor': "#E0E0E0",
+							'border': "1px solid #000"
+						});
+					},
+					dragLeave: function() {
+						$("#drop").html("Drop files here").css({
+							'backgroundColor': "#F6F6F6",
+							'border': "1px dashed #666"
+						});
+					},
+					progress: function(percentage) {
+						$("#drop").text("Uploading ("+percentage+"%)");
+					},
+					done: function(data) {
+						$("#status").text(JSON.stringify(data[0].url));
+						socket.emit('broadcast', data[0].url);	
+					}
+				});
+			window.addEventListener("keypress", newFileUploaded);
 			var status = document.getElementById('status');
 			var drop   = document.getElementById('drop');
 			var list   = document.getElementById('list');
@@ -68,42 +83,42 @@ Page = (function() {
 			  document.getElementById('status').innerHTML = 'Your browser does not support the HTML5 FileReader.';
 			}
 			
-			addEventHandler(drop, 'drop', function (e) {
-			  e = e || window.event; // get window.event if e argument missing (in IE)   
-			  if (e.preventDefault) { e.preventDefault(); } // stops the browser from redirecting off to the image.
-				console.log('asdf');
-			  var dt    = e.dataTransfer;
-			  var files = dt.files;
-			  for (var i=0; i<files.length; i++) {
-				var file = files[i];
-				var reader = new FileReader();
+			// addEventHandler(drop, 'drop', function (e) {
+			  // e = e || window.event; // get window.event if e argument missing (in IE)   
+			  // if (e.preventDefault) { e.preventDefault(); } // stops the browser from redirecting off to the image.
+				// console.log('asdf');
+			  // var dt    = e.dataTransfer;
+			  // var files = dt.files;
+			  // for (var i=0; i<files.length; i++) {
+				// var file = files[i];
+				// var reader = new FileReader();
 				  
-				//attach event handlers here...
+				// attach event handlers here...
 			   
-				reader.readAsDataURL(file);
-			  }
+				// reader.readAsDataURL(file);
+			  // }
 			  
 			
 			  
-			addEventHandler(reader, 'loadend', function(e, file) {
-				var bin           = this.result; 
-				var newFile       = document.createElement('div');
-				newFile.innerHTML = 'Loaded : '+file.name+' size '+file.size+' B';
-				list.appendChild(newFile);  
-				var fileNumber = list.getElementsByTagName('div').length;
+			// addEventHandler(reader, 'loadend', function(e, file) {
+				// var bin           = this.result; 
+				// var newFile       = document.createElement('div');
+				// newFile.innerHTML = 'Loaded : '+file.name+' size '+file.size+' B';
+				// list.appendChild(newFile);  
+				// var fileNumber = list.getElementsByTagName('div').length;
 				// status.innerHTML = fileNumber < files.length 
 								 // ? 'Loaded 100% of file '+fileNumber+' of '+files.length+'...' 
 								 // : 'Done loading. processed '+fileNumber+' files.';
-				status.innerHTML = bin;
-				socket.emit('broadcast', {fileObject: file, data: bin});
-				var img = document.createElement("img"); 
-				img.file = file;   
-				img.src = bin;
-				list.appendChild(img);
-			}.bindToEventHandler(file));
+				// status.innerHTML = bin;
+				// socket.emit('broadcast', {fileObject: file, data: bin});
+				// var img = document.createElement("img"); 
+				// img.file = file;   
+				// img.src = bin;
+				// list.appendChild(img);
+			// }.bindToEventHandler(file));
 			
-			  return false;
-			});
+			  // return false;
+			// });
 			
 			
 
