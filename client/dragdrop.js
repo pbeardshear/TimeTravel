4c714e34f01ae18238ae3bb3275312fbc17f97fe
Bryan Chu <bryanchu08@yahoo.com>
2012-08-11 20:38:49 -0700

    beep beep

--- client/dragdrop.js
+++ client/dragdrop.js
@@ -1,35 +1,17 @@
 Page = (function() {
-	function addEventHandler(obj, evt, handler) {
-		if(obj.addEventListener) {
-			// W3C method
-			obj.addEventListener(evt, handler, false);
-		} else if(obj.attachEvent) {
-			// IE method.
-			obj.attachEvent('on'+evt, handler);
-		} else {
-			// Old school method.
-			obj['on'+evt] = handler;
-		}
-	};
-	
-	Function.prototype.bindToEventHandler = function bindToEventHandler() {
-	  var handler = this;
-	  var boundParameters = Array.prototype.slice.call(arguments);
-	  //create closure
-	  return function(e) {
-		  e = e || window.event; // get window.event if e argument missing (in IE)   
-		  boundParameters.unshift(e);
-		  handler.apply(this, boundParameters);
-	  }
-	};
-	
+
 	function newFileUploaded(data) {
 		var name;
 		var list = document.getElementById('list');
-		var newEl = document.createElement('div');
+		var newEl = document.createElement('a');
+		newEl.href = data.url;
+		
 		var imgEl = document.createElement('img');
 		imgEl.src = "http://1.bp.blogspot.com/-ivx8sPkrN0E/T7oWeBCAwKI/AAAAAAAAAfw/aZoBDKIIB3o/s1600/File.png";
+		
 		var fileInfo = document.createElement('p');
+		fileInfo.innerHTML = data
+		
 		newEl.appendChild(imgEl);
 		newEl.appendChild(fileInfo);
 		
@@ -58,70 +40,14 @@ Page = (function() {
 					},
 					done: function(data) {
 						$("#status").text(JSON.stringify(data[0].url));
-						socket.emit('broadcast', { type: 'file', url: data[0].url });	
+						socket.emit('broadcast', { name: data[0].data.filename, size: data[0].data.size, fileType: data[0].data.type, type: 'file', url: data[0].url });
 					}
 				});
 			window.addEventListener("keypress", newFileUploaded);
+			socket.on('response', newFileUploaded);
 			var status = document.getElementById('status');
 			var drop   = document.getElementById('drop');
 			var list   = document.getElementById('list');
-			
-			if(window.FileReader) { 
-			  addEventHandler(window, 'load', function() {
-
-				
-				function cancel(e) {
-				  if (e.preventDefault) { e.preventDefault(); }
-				  return false;
-				}
-			  
-				// Tells the browser that we *can* drop on this target
-				addEventHandler(drop, 'dragover', cancel);
-				addEventHandler(drop, 'dragenter', cancel);
-			  });
-			} else { 
-			  document.getElementById('status').innerHTML = 'Your browser does not support the HTML5 FileReader.';
-			}
-			
-			// addEventHandler(drop, 'drop', function (e) {
-			  // e = e || window.event; // get window.event if e argument missing (in IE)   
-			  // if (e.preventDefault) { e.preventDefault(); } // stops the browser from redirecting off to the image.
-				// console.log('asdf');
-			  // var dt    = e.dataTransfer;
-			  // var files = dt.files;
-			  // for (var i=0; i<files.length; i++) {
-				// var file = files[i];
-				// var reader = new FileReader();
-				  
-				// attach event handlers here...
-			   
-				// reader.readAsDataURL(file);
-			  // }
-			  
-			
-			  
-			// addEventHandler(reader, 'loadend', function(e, file) {
-				// var bin           = this.result; 
-				// var newFile       = document.createElement('div');
-				// newFile.innerHTML = 'Loaded : '+file.name+' size '+file.size+' B';
-				// list.appendChild(newFile);  
-				// var fileNumber = list.getElementsByTagName('div').length;
-				// status.innerHTML = fileNumber < files.length 
-								 // ? 'Loaded 100% of file '+fileNumber+' of '+files.length+'...' 
-								 // : 'Done loading. processed '+fileNumber+' files.';
-				// status.innerHTML = bin;
-				// socket.emit('broadcast', {fileObject: file, data: bin});
-				// var img = document.createElement("img"); 
-				// img.file = file;   
-				// img.src = bin;
-				// list.appendChild(img);
-			// }.bindToEventHandler(file));
-			
-			  // return false;
-			// });
-			
-			
-
 		}
 	};
 })();
